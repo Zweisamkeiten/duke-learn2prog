e5dba508190cda73b9583bbd6938fb68f3dabe80
Coursera Learner <nobody@nowhere.nul>
2022-10-11 03:14:27 +0000

    retirement.c

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _retire_info {
+struct _retire_info {
   int months;
   double contribution, rate_of_return;
-} retire_info;
+};
 
-void retirement (int startAge,      // in months
-		 double initial,    // initial savings in dollars
-		 retire_info working, // info about working
-		 retire_info retired) // info about being retired
+typedef struct _retire_info retire_info;
+
+void retirement(int startAge,        // in months
+                double initial,      // initial savings in dollars
+                retire_info working, // info about working
+                retire_info retired) // info about being retired
 {
   int ageMonths = startAge;
   double balance = initial;
@@ -17,10 +19,12 @@ void retirement (int startAge,      // in months
   int endAge = startAge + working.months + retired.months;
   while (ageMonths < endAge) {
     printf("Age %3d month %2d you have $%.2lf\n", ageMonths / 12,
-	   ageMonths % 12, balance);
+           ageMonths % 12, balance);
     ageMonths += 1;
-    double rate_of_return = ageMonths <= retiredAge ? working.rate_of_return : retired.rate_of_return;
-    double contribution = ageMonths <= retiredAge ? working.contribution : retired.contribution;
+    double rate_of_return = ageMonths <= retiredAge ? working.rate_of_return
+                                                    : retired.rate_of_return;
+    double contribution =
+        ageMonths <= retiredAge ? working.contribution : retired.contribution;
     balance = balance * (1 + rate_of_return / 12) + contribution;
   }
 }
