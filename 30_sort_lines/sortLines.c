b97e106b84ff433548c1c80226e55a970b6d12b3
Coursera Learner <nobody@nowhere.nul>
2022-10-20 02:44:52 +0000

    30_sort_lines

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -16,9 +16,43 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void readTheInput(FILE * stream, char *** lines, size_t * length) {
+    char * curr = NULL;
+    size_t sz;
+    while (getline(&curr, &sz, stream) >= 0) {
+        *lines = realloc(*lines, (*length+1) * sizeof(**lines));
+        (*lines)[*length] = curr;
+        curr = NULL;
+        *length = *length + 1;
+    }
+    free(curr);
+}
+
 int main(int argc, char ** argv) {
-  
-  //WRITE YOUR CODE HERE!
-  
-  return EXIT_SUCCESS;
+    char ** lines = NULL;
+    size_t length = 0;
+    if (argc == 1) {
+        readTheInput(stdin, &lines, &length);
+    }
+    else {
+        for (int i = 1; i < argc; ++i) {
+            FILE * f = fopen(argv[i], "r");
+            if (f == NULL) {
+                perror("Could not open file");
+                return EXIT_FAILURE;
+            }
+            readTheInput(f, &lines, &length);
+            if (fclose(f) != 0) {
+                perror("Failed to close the input file!");
+                return EXIT_FAILURE;
+            }
+        }
+    }
+    sortData(lines, length);
+    for (size_t j = 0; j < length; ++j) {
+        printf("%s", lines[j]);
+        free(lines[j]);
+    }
+    free(lines);
+    return EXIT_SUCCESS;
 }
